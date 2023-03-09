b087df87307a5c9a25c266acf92c3d5477526fb9
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-03-09 13:11:01 +0100

    commit code samples that have alreday been completed

---
+++ FactorialCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+namespace FactorialCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factorial Program in C#: Factorial of n is the product of all positive descending integers. Factorial of n is denoted by n!.
+            // 4! = 4*3*2*1 = 24    
+            // 6! = 6*5*4*3*2*1 = 720 
+            int factorial = 1;
+
+            //Get user input
+            Write("Enter any Number: ");
+            int.TryParse(Console.ReadLine(), out int numberToCalculate);
+
+            //Calculate factorial
+            for (int i = 1; i <= numberToCalculate; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            //Print Result
+            WriteLine($"The factorial of {numberToCalculate} is {factorial}");
+        }
+    }
+}
---
+++ WordOccurrencesFinder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+// See https://aka.ms/new-console-template for more information
+string text = File.ReadAllText("input.txt").ToLower();
+
+int theWord = Regex.Matches(text, @"\bthe\b").Count;
+WriteLine($"The word 'The' has {theWord} ocurrences in the text");
+
+Write("write a word for the search: ");
+string? wordToSearch = Console.ReadLine();
+int wordCounter = Regex.Matches(text, $@"\b{wordToSearch}\b").Count;
+WriteLine($"\nThe word {wordToSearch} has {wordCounter} ocurrences in the text");
