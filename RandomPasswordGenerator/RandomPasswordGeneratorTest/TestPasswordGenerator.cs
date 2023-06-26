641348164f9a1927b479adfcb3bb48e8e082cab3
Cecilia Coutinho <cecicoutinho@gmail.com>
2023-06-26 13:46:23 +0200

    generate password with min and max lenght passed

--- RandomPasswordGenerator/RandomPasswordGeneratorTest/TestPasswordGenerator.cs
+++ RandomPasswordGenerator/RandomPasswordGeneratorTest/TestPasswordGenerator.cs
@@ -28,14 +28,58 @@ namespace RandomPasswordGeneratorTest
             //Arrange
             RandomNumberGenerator randomGenerator = RandomNumberGenerator.Create();
             PasswordGenerator passGenerator = new PasswordGenerator();
-            int minInput = 8;
-            int maxInput = 12;
+            int minInput = 10;
+            int maxInput = 20;
 
             //Act
             int randomIntWithinRange = passGenerator.GetRandomIntWithinRange(randomGenerator, minInput, maxInput);
 
             //Assert
             randomIntWithinRange.Should().BeInRange(minInput, maxInput);
+        }
+
+        [Fact]
+        public void GeneratePassword_Should_Return_Valid_Password()
+        {
+            //Arrange
+            int minLength = 8;
+            int maxLength = 12;
+            char[] validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+[{]}:>|/?".ToCharArray();
+            //PasswordGenerator passGenerator = new PasswordGenerator();
+
+            //Act
+            string password = GeneratePassword(minLength, maxLength);
+
+            //Assert
+            password.Should().NotBeNullOrEmpty();
+            password.Should().NotBeNullOrWhiteSpace();
+            password.Length.Should().BeGreaterThanOrEqualTo(minLength);
+            password.Length.Should().BeLessThanOrEqualTo(maxLength);
+            password.ToCharArray().All(c => validCharacters.Contains(c)).Should().BeTrue();
+        }
+
+        string GeneratePassword(int minLength, int maxLength)
+        {
+            PasswordGenerator passGenerator = new PasswordGenerator();
+            char[] validCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-+[{]}:>|/?".ToCharArray();
+
+            int minLengthIndex = 0;
+            int maxLengthIndex = validCharacters.Length - 1;
+
+            using (var randomNumberGenerator = RandomNumberGenerator.Create())
+            {
+                var passwordLength = passGenerator.GetRandomIntWithinRange(randomNumberGenerator, minLength, maxLength);
+                var password = new char[passwordLength];
+
+                for (int i = 0; i < password.Length; i++)
+                {
+                    int randomIndex = passGenerator.GetRandomIntWithinRange(randomNumberGenerator, minLengthIndex, maxLengthIndex);
+                    char randomCharacter = validCharacters[randomIndex];
+                    password[i] = randomCharacter;
+                }
+                return new string(password);
+            }
+
         }
 
     }
